f8197bcc0a0e5b62a42d40e0ac8077bed140f199
RamenRice <57263064+RamenRice@users.noreply.github.com>
2021-03-05 10:50:12 -0500

    Update SimplePassSystem.py

--- SimplePassSystem.py
+++ SimplePassSystem.py
@@ -1,3 +1,4 @@
+import sys
 start = input('Would you like to make an account? : ').lower()
 username_and_password = []
 if start == 'yes' or start == 'y':
@@ -8,18 +9,18 @@ if start == 'yes' or start == 'y':
     username_and_password.append(password)
 else:
     print('Come back when you want to make an account.')
-    break
+    sys.exit()
 test_login = input('Would you like to login? : ').lower()
 while start == 'yes' or start == 'y':
     if test_login == 'n' or test_login == 'no':
-        break
+        sys.exit()
     if test_login == 'y' or test_login == 'yes':
         username_login = input('Type in your username : ')
         password_login = input('Type in your password: ')
         if username_login in username_and_password:
             if password_login in username_and_password:
                 print('Succesfull login')
-                break
+                sys.exit()
             if password_login not in username_and_password:
                 print('Unsuccessfull login')
                 test_login = input('Would you like to retry login? : ').lower()
@@ -27,4 +28,4 @@ while start == 'yes' or start == 'y':
             print('Unsuccessfull login')
             test_login = input('Would you like to retry login? : ').lower()
     else:
-        break
+        sys.exit()
